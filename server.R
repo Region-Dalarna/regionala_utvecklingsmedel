f451a24010bc7cd9d258d61a186c2d5f78e60660
Klerby Anna /Regional utvecklingsförvaltning /Falun <se2321000180-5kfl@ltdalarna.se>
2026-07-24 19:29:26 +0200

    Flera nya diagram.

--- server.R
+++ server.R
@@ -1,47 +1,100 @@
 shinyServer(function(input, output, session) {
 
-  output$stapeldiagram <- renderPlot({
+# FLIKEN Samlade 1:1-medel
+
+  output$stapeldiagram_ar_stod <- renderPlot({
+    req(input$valda_stodtyper, input$valda_ar)
     data_trans %>%
       filter(stodtyp %in% input$valda_stodtyper) %>%
-      group_by(beslut_ar) %>%
-      summarise(summa = sum(beviljat_belopp, na.rm = TRUE)) %>%
-      ggplot(aes(x = factor(beslut_ar), y = summa)) +
-      geom_col(fill = "steelblue") +
+      group_by(beslut_ar, stodtyp) %>%
+      summarise(summa = sum(beviljat_belopp, na.rm = TRUE), .groups = "drop") %>%
+      ggplot(aes(x = factor(beslut_ar), y = summa, fill = stodtyp)) +
+      geom_col(position = "dodge") +
+      scale_fill_manual(
+        values = c("PROJ" = "steelblue",
+                   "FTG"  = "darkorange",
+                   "KS"   = "forestgreen"),
+        labels = c("PROJ" = "Projektmedel",
+                   "FTG"  = "Företagsstöd",
+                   "KS"   = "Kommersiell service")
+      ) +
       labs(
         title = "Beviljat belopp per år",
         x = "År",
-        y = "Summa beviljat belopp (kr)"
-      ) +
-      theme_minimal()
+        y = "Summa beviljat belopp (kr)",
+        fill = "Stödtyp"
+    ) +
+    theme_minimal()
   })
-})
 
+# FLIKEN PROJEKTMEDEL
 
-# Filtrerat dataset för företagsstöd
-ftg_data <- reactive({
-  df <- data_trans %>% filter(stodtyp == "FTG")
+  #Filtrerat dataset för projektstöd
+  proj_data <- reactive({
+    data_trans %>%
+      filter(stodtyp == "PROJ",
+             beslut_ar %in% input$proj_ar)
+  })
 
-  if (input$ftg_ar != "Alla år") {
-    df <- df %>% filter(beslut_ar == input$ftg_ar)
-  }
-  df
-})
+  # Diagram beviljade och utbetalada medel
+  output$proj_ar_diagram <- renderPlot({
+
+    if (input$proj_matt == "belopp") {
+      plot_data <- proj_data() %>%
+        group_by(beslut_ar) %>%
+        summarise(varde = sum(beviljat_belopp, na.rm = TRUE))
+      y_label <- "Summa beviljat belopp (kr)"
+
+    } else {
+      plot_data <- proj_data() %>%
+        group_by(beslut_ar) %>%
+        summarise(varde = n_distinct(arende))
+      y_label <- "Antal ärenden"
+
+    }
+
+    plot_data %>%
+      ggplot(aes(x = beslut_ar, y = varde, fill = beslut_ar)) +
+      geom_col(fill = "steelblue") +
+      labs(
+        title = "Projektmedel per kalenderår",
+        x = "År",
+        y = y_label
+      ) +
+      theme_minimal() +
+      theme(legend.position = "none")
+    })
+
+# FLIKEN FÖRETAGSSTÖD
+
+  #Filtrerat dataset för företagsstöd
+  ftg_data <- reactive({
+    data_trans %>%
+      filter(stodtyp == "FTG",
+             beslut_ar %in% input$ftg_ar)
+  })
 
 # Diagram könsfördelning VD
 output$ftg_kon_diagram <- renderPlot({
 
   if (input$ftg_matt == "belopp") {
     plot_data <- ftg_data() %>%
-      group_by(vd_kon) %>%
+      group_by(vd_kon_ren) %>%
       summarise(varde = sum(beviljat_belopp, na.rm = TRUE))
     y_label <- "Summa beviljat belopp (kr)"
 
-  } else {
+  } else if (input$ftg_matt == "antal") {
     plot_data <- ftg_data() %>%
-      group_by(vd_kon) %>%
+      group_by(vd_kon_ren) %>%
       summarise(varde = n_distinct(arende))
     y_label <- "Antal ärenden"
-  }
+
+    } else {
+  plot_data <- ftg_data() %>%
+    group_by(vd_kon_ren) %>%
+    summarise(varde = sum(utbet_belopp, na.rm = TRUE))
+  y_label <- "Utbetalt belopp"
+}
 
   plot_data %>%
     ggplot(aes(x = vd_kon_ren, y = varde, fill = vd_kon_ren)) +
@@ -52,15 +105,155 @@ output$ftg_kon_diagram <- renderPlot({
       y = y_label,
       fill = "Kön"
     ) +
-    theme_minimal()
+    theme_minimal() +
+    theme(legend.position = "none")  # tar bort legenden
+})
+
+# Branschfördelning (obs! branscherna är sammanslagna och översatta SNI 2007 + 2025)
+output$ftg_bransch_diagram <- renderPlot({
+
+  if (input$ftg_matt == "belopp") {
+    plot_data <- ftg_data() %>%
+      group_by(bransch_avd_2025) %>%
+      summarise(varde = sum(beviljat_belopp, na.rm = TRUE))
+    y_label <- "Summa beviljat belopp (kr)"
+
+  } else if (input$ftg_matt == "antal") {
+    plot_data <- ftg_data() %>%
+      group_by(bransch_avd_2025) %>%
+      summarise(varde = n_distinct(arende))
+    y_label <- "Antal ärenden"
+
+  } else {
+  plot_data <- ftg_data() %>%
+    group_by(bransch_avd_2025) %>%
+    summarise(varde = sum(utbet_belopp, na.rm = TRUE))
+  y_label <- "Utbetalt belopp"
+}
+  plot_data %>%
+    mutate(bransch_kort = str_trunc(bransch_avd_2025, width = 20, ellipsis = "...")) %>%
+    ggplot(aes(x = reorder(bransch_kort, varde), y = varde, fill = bransch_kort)) +
+    geom_col() +
+    coord_flip() +
+    labs(
+      title = "Fördelning per bransch",
+      x = "Bransch",
+      y = y_label
+    ) +
+    theme_minimal() +
+    theme(legend.position = "none")
+})
+
+# Kommunfördelning
+output$ftg_kommun_diagram <- renderPlot({
+
+  if (input$ftg_matt == "belopp") {
+    plot_data <- ftg_data() %>%
+      group_by(arbetsstalle) %>%
+      summarise(varde = sum(beviljat_belopp, na.rm = TRUE))
+    y_label <- "Summa beviljat belopp (kr)"
+
+  } else if (input$ftg_matt == "antal") {
+    plot_data <- ftg_data() %>%
+      group_by(arbetsstalle) %>%
+      summarise(varde = n_distinct(arende))
+    y_label <- "Antal ärenden"
+
+  }  else {
+  plot_data <- ftg_data() %>%
+    group_by(arbetsstalle) %>%
+    summarise(varde = sum(utbet_belopp, na.rm = TRUE))
+  y_label <- "Utbetalt belopp"
+}
+  plot_data %>%
+    ggplot(aes(x = reorder(arbetsstalle, varde), y = varde, fill = arbetsstalle)) +
+    geom_col() +
+    coord_flip() +
+    labs(
+      title = "Fördelning per kommun",
+      x = "Kommun",
+      y = y_label
+    ) +
+    theme_minimal() +
+    theme(legend.position = "none")
+})
+
+# Fördelning lokal klassificering 1
+output$ftg_lokal_1_diagram <- renderPlot({
+
+  if (input$ftg_matt == "belopp") {
+    plot_data <- ftg_data() %>%
+      group_by(lokal_1) %>%
+      summarise(varde = sum(beviljat_belopp, na.rm = TRUE))
+    y_label <- "Summa beviljat belopp (kr)"
+
+  } else if (input$ftg_matt == "antal") {
+    plot_data <- ftg_data() %>%
+      group_by(lokal_1) %>%
+      summarise(varde = n_distinct(arende))
+    y_label <- "Antal ärenden"
+
+    } else {
+  plot_data <- ftg_data() %>%
+    group_by(lokal_1) %>%
+    summarise(varde = sum(utbet_belopp, na.rm = TRUE))
+  y_label <- "Utbetalt belopp"
+}
+  plot_data %>%
+    ggplot(aes(x = reorder(lokal_1, varde), y = varde, fill = lokal_1)) +
+    geom_col() +
+    coord_flip() +
+    labs(
+      title = "Fördelning klassificering 1",
+      x = "Klassificering",
+      y = y_label
+    ) +
+    theme_minimal() +
+    theme(legend.position = "none")
 })
 
 
-#shinyServer(function(input, output, session) {
 
-  # Filtrera data baserat på användarens val
-  #data_filtered <- reactive({
-   # data_trans %>%
-    #  filter(beslut_ar == input$valt_ar)
-  #})
-#})
+# Filtrera för kommersiell service
+ks_data <- reactive({
+  data_trans %>%
+    filter(stodtyp == "KS",
+           beslut_ar %in% input$ks_ar)
+})
+
+# Kommunfördelning
+output$ks_kommun_diagram <- renderPlot({
+
+  if (input$ks_matt == "belopp") {
+    plot_data <- ks_data() %>%
+      group_by(arbetsstalle) %>%
+      summarise(varde = sum(beviljat_belopp, na.rm = TRUE))
+    y_label <- "Summa beviljat belopp (kr)"
+
+  } else if (input$ks_matt == "antal"){
+    plot_data <- ks_data() %>%
+      group_by(arbetsstalle) %>%
+      summarise(varde = n_distinct(arende))
+    y_label <- "Antal ärenden"
+
+    } else {
+    plot_data <- ks_data() %>%
+      group_by(arbetsstalle) %>%
+      summarise(varde = sum(utbet_belopp, na.rm = TRUE))
+    y_label <- "Utbetalt belopp"
+  }
+  plot_data %>%
+    ggplot(aes(x = reorder(arbetsstalle, varde), y = varde, fill = arbetsstalle)) +
+    geom_col() +
+    coord_flip() +
+    labs(
+      title = "Fördelning per kommun",
+      x = "Kommun",
+      y = y_label
+    ) +
+    theme_minimal() +
+    theme(legend.position = "none")
+} )
+
+} )
+
--- ui.R
+++ ui.R
@@ -19,50 +19,129 @@ shinyUI(
     ),
     tabsetPanel(
       tabPanel('Beviljade 1:1-medel',
+               fluidRow(
+               column(3,
+               checkboxGroupInput(
+                 inputId  = "valda_ar",
+                 label    = "Välj år:",
+                 choices  = sort(unique(data_trans$beslut_ar)),
+                 selected = sort(unique(data_trans$beslut_ar))
+               ),
+
                checkboxGroupInput(
                  inputId  = "valda_stodtyper",
                  label    = "Välj stödtyp:",
-                 choices = c("Projektmedel"       = "PROJ",
-                             "Företagsstöd"       = "FTG",
+                 choices = c("Projektmedel"        = "PROJ",
+                             "Företagsstöd"        = "FTG",
                              "Kommersiell service" = "KS"),
-                 selected = c("Projektmedel", "Företagsstöd", "Kommersiell service")  # alla förkryssade från start
+                 selected = c("PROJ", "FTG", "KS")
+                 )
                ),
+
+               column(9,
                h3('Beviljat belopp per år'),
-               plotOutput('stapeldiagram')
-      ),
+               plotOutput('stapeldiagram_ar_stod')
+              )
+          )
+        ),
       tabPanel('Projektmedel',
-               h3('Beviljat belopp per år'),
-               plotOutput('stapeldiagram')
+               # Rad 1 - filter + två diagram
+               fluidRow(
+                 column(3,
+                        checkboxGroupInput(
+                          inputId  = "proj_ar",
+                          label    = "Välj år:",
+                          choices  = sort(unique(data_trans$beslut_ar)),
+                          selected = sort(unique(data_trans$beslut_ar))
+                        ),
+                        radioButtons(
+                          inputId  = "proj_matt",
+                          label    = "Visa som:",
+                          choices  = c("Beviljat belopp" = "belopp", "Antal ärenden" = "antal"),
+                          selected = c("belopp")
+                        )
+                 ),
+                 column(9,
+                h3('Beviljade projektmedel per kalenderår'),
+                plotOutput('proj_ar_diagram')
+                )
+          )
       ),
 
       tabPanel('Företagsstöd',
+               # Rad 1 - filter + två diagram
                fluidRow(
                  column(3,
-                        selectInput(
+                        checkboxGroupInput(
                           inputId  = "ftg_ar",
                           label    = "Välj år:",
-                          choices  = c("Alla år", sort(unique(data_trans$beslut_ar))),
-                          selected = "Alla år"
+                          choices  = sort(unique(data_trans$beslut_ar)),
+                          selected = sort(unique(data_trans$beslut_ar))
                         ),
                         radioButtons(
                           inputId  = "ftg_matt",
                           label    = "Visa som:",
-                          choices  = c("Belopp" = "belopp", "Antal ärenden" = "antal"),
-                          selected = "belopp"
+                          choices  = c("Beviljat belopp" = "belopp", "Antal ärenden" = "antal", "Utbetalt belopp" = "utbet"),
+                          selected = c("belopp")
                         )
                  ),
-                 column(9,
-                        h3("Fördelning kön på VD"),
+                 column(4,
+                        h3("Fördelning efter VD:ns kön"),
                         plotOutput("ftg_kon_diagram")
+                 ),
+                 column(5,
+                        h3("Fördelning per bransch"),
+                        plotOutput("ftg_bransch_diagram")
+                 )
+               ),
+               # Rad 2 - två diagram
+               fluidRow(
+                 column(6,
+                        h3("Fördelning per kommun"),
+                        plotOutput("ftg_kommun_diagram")
+                 ),
+                 column(6,
+                        h3("Fördelning lokal klassificering 1"),
+                        plotOutput("ftg_lokal_1_diagram")
                  )
                )
       ),
+
       tabPanel('Kommersiell service',
-               h3('Beviljat belopp per år'),
-               plotOutput('stapeldiagram')
-      ),
-      tabPanel('Om', p('Beskriv applikationen här.'))
+               fluidRow(
+                 column(3,
+                        checkboxGroupInput(
+                          inputId  = "ks_ar",
+                          label    = "Välj år:",
+                          choices  = sort(unique(data_trans$beslut_ar)),
+                          selected = sort(unique(data_trans$beslut_ar))
+                        ),
+                        radioButtons(
+                          inputId  = "ks_matt",
+                          label    = "Visa som:",
+                          choices  = c("Beviljat belopp" = "belopp",
+                                       "Antal ärenden" = "antal",
+                                       "Utbetalt belopp" = "utbet"),
+                          selected = c("belopp")
+                        )
+                 ),
+
+                 column(9,
+                      h3('Belopp per kalenderår'),
+                      plotOutput('ks_kommun_diagram')
+    )
+    )
     ),
+
+      tabPanel('Om',
+      p('Beskriv applikationen här. Viktigt att notera:
+                       Beviljade belopp registreras vid den tidpunkt då beslut fattas. Själva utbetalningen sker vid en eller flera senare tillfällen.
+                       För företagsstöd görs normalt sett en utbetalning, men det förekommer att den sker kalenderåret efter ansökan beviljades.
+                       För projektstöd görs utbetalningar ungefär var 4:e månad. Projektens löptid är allt ifrån 3 månader till 3 år, ibland med förlängning.
+                       De beviljade totalsummorna för projektmedel under ett visst år kan förväntas betalas ut under en treårsperiod.
+                       Medlen ska även motiveras med faktiska kostnader, varför det kan finnas skillnad mellan beviljat belopp och faktiskt utbetalt belopp.'))
+    ),
+
     tags$div(
       class = 'rd-footer',
       'Samhällsanalys, Region Dalarna · ',
@@ -73,55 +152,3 @@ shinyUI(
     )
   )
 )
-  #source('global.R')
-# shinyUI(
-#   fluidPage(
-#     tags$head(
-#       tags$link(rel = 'icon', type = 'image/x-icon', href = 'favicon.ico'),
-#       tags$link(rel = 'stylesheet', type = 'text/css', href = 'regiondalarna_ruf.css'),
-#       tags$link(rel = 'stylesheet', type = 'text/css', href = 'app.css')
-#     ))
-#   ),
-#
-# # ---- Header (matchar .rd-header i regiondalarna_ruf.css) --------------
-#     tags$div(
-#       class = 'rd-header',
-#       tags$div(class = 'rd-header__title', 'regionala_utvecklingsmedel'),
-#       tags$a(
-#         class  = 'rd-header__right',
-#         href   = 'https://www.regiondalarna.se',
-#         target = '_blank',
-#         tags$img(src = 'logo_liggande_fri_vit.png', alt = 'Region Dalarna'),
-#         tags$span('Samhällsanalys')
-#       )
-#     ),
-#
-#     # ---- Innehåll ---------------------------------------------------------
-#     tabsetPanel(
-#       tabPanel('Tab 1',
-#         h3('Hej från regionala_utvecklingsmedel'),
-#         verbatimTextOutput('På de här sidorna kan du fördjupa dig i hur de regionala utvecklingsmedlen fördelas till företag och till regionala utvecklingsprojekt')
-#       ),
-#       tabPanel('Om', p('Då testar jag att beskriva detta här. Beskriv applikationen här.'))
-#     ),
-# tabsetPanel(
-#   tabPanel('Beviljat belopp',
-#            h3('Beviljat belopp per år'),
-#            plotOutput('stapeldiagram')
-#   ),
-#
-#
-#     # ---- Footer (matchar .rd-footer i regiondalarna_ruf.css) --------------
-#     tags$div(
-#       class = 'rd-footer',
-#       'Samhällsanalys, Region Dalarna · ',
-#       tags$a(
-#         href = 'mailto:samhallsanalys@regiondalarna.se',
-#         'samhallsanalys@regiondalarna.se'
-#       )
-#     )
-#   )
-# )
-
-
-
